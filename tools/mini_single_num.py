1d24765af8f8fad70b15656c9f1335563e350763
Daiju Nakayama <42.daiju@gmail.com>
2018-02-24 20:30:44 +0900

    単一の値生成スクリプトのバグ修正

--- tools/mini_single_num.py
+++ tools/mini_single_num.py
@@ -1,16 +1,29 @@
 import sys
+from math import sqrt
 from functools import lru_cache
 from itertools import cycle
 
 
 @lru_cache(maxsize=None)
-def factrize(n, s):
-    return [i for i in range(s, n + 1) if n % i == 0]
+def factrize(n):
+    ret = []
+    i = 2
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            ret.append(i)
+            j = n // i
+            if j != i:
+                ret.append(j)
+    return ret
 
 
 class Code:
+
+    MUL_OVERHEAD = 6
+
     def __init__(self, bits=8):
         self.code = []
+        self.log = []
         self.num = 0
         self._move = 0
         self.cell_size = 1 << bits
@@ -18,6 +31,7 @@ class Code:
     def copy(self):
         code = Code()
         code.code = self.code[:]
+        code.log = self.log[:]
         code.num = self.num
         code._move = self._move
         code.cell_size = self.cell_size
@@ -36,8 +50,10 @@ class Code:
         self.num = (self.num + n) % self.cell_size
         if n < 0:
             self.code.append(['-', -n])
+            self.log.append(f'-{-n}')
         elif n > 0:
             self.code.append(['+', n])
+            self.log.append(f'+{n}')
 
     def __add__(self, n):
         code = self.copy()
@@ -56,6 +72,7 @@ class Code:
             self.code.append(['-', n])
             self.code.append(['%s]%s' % (self.move, self.move), 1])
         self.num = (self.num * n) % self.cell_size
+        self.log.append(f'*{n}')
 
     def __mul__(self, n):
         code = self.copy()
@@ -74,44 +91,57 @@ class Code:
         return ''.join(s * n for s, n in self.code)
 
 
-best_score = 0
-best_code = None
+class DepthFirstSearch:
 
-def dfs(num, code, depth):
-    global best_score, best_code
-    num = num % code.cell_size
-    d = min([code.num - num, -(code.cell_size - num + code.num)], key=abs)
-    best = code + d
-    if best_code is None or best.score < best_score:
-        best_score = best.score
-        best_code = best
-    if best.score > best_score or depth <= 0:
-        return
-    max_range = min(num, 100)
-    for a in range(max_range, -max_range-1, -1):
-        c = code + a
-        if c.score >= best_score:
-            continue
-        if c.num == 0:
-            continue
-        for m in range(2, max_range+1):
-            dfs(num, c * m, depth - 1)
+    def __init__(self, null_code, goal, max_depth):
+        self.null_code = null_code
+        self.goal = goal % self.null_code.cell_size
+        self.max_depth = max_depth
+        self.best_code = None
+        self.best_score = None
+
+    def comp(self, code):
+        d = min([self.goal - code.num, self.goal - code.num - code.cell_size], key=abs)
+        code = code + d
+        if self.best_code is None or code.score < self.best_score:
+            self.best_code = code
+            self.best_score = code.score
+
+    def dfs(self):
+        self._dfs(self.null_code, 1)
+        return self.best_code
+
+    def _dfs(self, code, depth):
+        self.comp(code)
+        if depth >= self.max_depth:
+            return
+        add = 0
+        while code.score + abs(add) + 2 + code.MUL_OVERHEAD < self.best_score:
+            added = code + add
+            if abs(added.num) > 1:
+                mul = 2
+                while added.score + mul + code.MUL_OVERHEAD < self.best_score:
+                    self._dfs(added * mul, depth + 1)
+                    mul += 1
+            if add <= 0:
+                add = -add + 1
+            else:
+                add = -add
 
 
 def main():
-    global best_score, best_code
     n = int(sys.argv[1])
     bits = int(sys.argv[2]) if len(sys.argv) > 2 else 8
     pre = ''
-    for max_depth in range(5):
-        best_score = 0
-        best_code = None
-        dfs(n, Code(bits), max_depth)
-        assert best_code.score == best_score
-        cs = str(best_code)
-        if cs != pre:
-            print(max_depth, best_score, cs)
-            pre = cs
+    for max_depth in range(1, 5):
+        dfs = DepthFirstSearch(Code(bits), n, max_depth)
+        code = dfs.dfs()
+        cs = str(code)
+        if cs != pre and len(cs) < 1000:
+            print(max_depth, code.score, cs, code.log)
+        else:
+            print(max_depth, code.score)
+        pre = cs
 
 
 if __name__ == '__main__':
